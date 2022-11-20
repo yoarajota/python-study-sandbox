26cc145d274e90f836a29a454a5449f935fadc90
yoarajota <joaovbscontato@gmail.com>
2022-11-20 16:17:25 -0300

    fix procfile name again

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import config
 
 client = tweepy.Client(config.BEARER_TOKEN, config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
-query = 'from:ioweasy -is:retweet'
+query = 'from:Z3N0n_ -is:retweet'
 response = client.search_recent_tweets(query=query)
             # o all é para a api premium ou academica algo assim
 
