45bea478a324de456784fcdce2ae9ba12eecd740
yoarajota <joaovbscontato@gmail.com>
2022-11-20 17:51:21 -0300

    search to test if already have any resposne by me in the recent tweet

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ response = client.search_recent_tweets(query=query)
 
 tweetId = response.data[0].id
 
-auth = tweepy.OAuth1UserHandler(config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+query_replys_of_recent_tweet = 'from:yoarajota in_reply_to_tweet_id:{}'.format(tweetId)
+replys_of_recent_tweet = client.search_recent_tweets(query=query_replys_of_recent_tweet).data
 
-api = tweepy.API(auth)
+if (replys_of_recent_tweet is None):
+    auth = tweepy.OAuth1UserHandler(config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
 
-try_to_get_my_last_tweet_query = 'from:{}'.format(client.get_me().data.id)+' -is:retweet is:reply'
-last_tweet = client.search_recent_tweets(query=try_to_get_my_last_tweet_query).data[0]
+    try_to_get_my_last_tweet_query = 'from:{}'.format(client.get_me().data.id)+' -is:retweet is:reply'
+    last_tweet = client.search_recent_tweets(query=try_to_get_my_last_tweet_query).data[0]
 
-if (re.search('fodase', last_tweet.text)):
-    client.create_tweet(in_reply_to_tweet_id=tweetId, text='fodase')
-else:
-    client.create_tweet(in_reply_to_tweet_id=tweetId, text='nao pedi')
+    if (re.search('fodase', last_tweet.text)):
+        #client.create_tweet(in_reply_to_tweet_id=tweetId, text='fodase')
+        print('a')
+    else:
+        print('b')
+        #client.create_tweet(in_reply_to_tweet_id=tweetId, text='nao pedi')
