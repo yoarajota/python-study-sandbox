ed9d8e6d2eabe0d7b6f0b12345c2102524b583ed
yoarajota <joaovbscontato@gmail.com>
2022-11-20 18:45:24 -0300

    sleep and loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy
 import config
 import re
+import time
 
 client = tweepy.Client(config.BEARER_TOKEN, config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
@@ -12,15 +13,16 @@ tweetId = response.data[0].id
 
 query_replys_of_recent_tweet = 'from:yoarajota in_reply_to_tweet_id:{}'.format(tweetId)
 replys_of_recent_tweet = client.search_recent_tweets(query=query_replys_of_recent_tweet).data
+while True:
+    if (replys_of_recent_tweet is None):
+        auth = tweepy.OAuth1UserHandler(config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
 
-if (replys_of_recent_tweet is None):
-    auth = tweepy.OAuth1UserHandler(config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
+        try_to_get_my_last_tweet_query = 'from:{}'.format(client.get_me().data.id)+' -is:retweet is:reply'
+        last_tweet = client.search_recent_tweets(query=try_to_get_my_last_tweet_query).data[0]
 
-    try_to_get_my_last_tweet_query = 'from:{}'.format(client.get_me().data.id)+' -is:retweet is:reply'
-    last_tweet = client.search_recent_tweets(query=try_to_get_my_last_tweet_query).data[0]
-
-    if (re.search('fodase', last_tweet.text)):
-        client.create_tweet(in_reply_to_tweet_id=tweetId, text='fodase')
-    else:
-        client.create_tweet(in_reply_to_tweet_id=tweetId, text='nao pedi')
+        if (re.search('fodase', last_tweet.text)):
+            client.create_tweet(in_reply_to_tweet_id=tweetId, text='fodase')
+        else:
+            client.create_tweet(in_reply_to_tweet_id=tweetId, text='nao pedi')
+        time.sleep(1500)
