ef81732ce5c399400c268606c0b92f3fee8dcaa6
yoarajota <joaovbscontato@gmail.com>
2022-11-20 17:16:44 -0300

    search to donot have excpetion that last tweet is repeated

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import tweepy
 import config
+import re
 
 client = tweepy.Client(config.BEARER_TOKEN, config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
-query = 'from:Z3N0n_ -is:retweet'
+query = 'from:ioweasy -is:retweet'
 response = client.search_recent_tweets(query=query)
             # o all é para a api premium ou academica algo assim
 
 tweetId = response.data[0].id
 
 auth = tweepy.OAuth1UserHandler(config.API_KEY, config.API_SCRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+
 api = tweepy.API(auth)
 
-client.create_tweet(in_reply_to_tweet_id=tweetId, text='fodaseeeeeeee')
+try_to_get_my_last_tweet_query = 'from:{}'.format(client.get_me().data.id)+' -is:retweet is:reply'
+last_tweet = client.search_recent_tweets(query=try_to_get_my_last_tweet_query).data[0]
+
+if (re.search('fodase', last_tweet.text)):
+    client.create_tweet(in_reply_to_tweet_id=tweetId, text='fodase')
+else:
+    client.create_tweet(in_reply_to_tweet_id=tweetId, text='nao pedi')
