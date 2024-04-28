99ee323756d99df64f02fca81693781565d2a268
yoarajota <joaovbscontato@gmail.com>
2024-04-28 17:05:13 -0300

    model function

--- Stanford - Machine Learning Specialization/Supervised Machine Learning Regression and Classification/Model representation.py
+++ Stanford - Machine Learning Specialization/Supervised Machine Learning Regression and Classification/Model representation.py
@@ -20,3 +20,120 @@ x_train = np.array([1.0, 2.0])
 y_train = np.array([300.0, 500.0])
 print(f"x_train = {x_train}")
 print(f"y_train = {y_train}")
+
+# Number of training examples m
+
+# You will use m to denote the number of training examples. 
+# Numpy arrays have a .shape parameter. x_train.shape returns a python tuple with an entry for each dimension. x_train.shape[0] is the length of the array and number of examples as shown below.
+
+# m is the number of training examples
+print(f"x_train.shape: {x_train.shape}")
+m = x_train.shape[0]
+print(f"Number of training examples is: {m}")
+
+# One can also use the Python len() function as shown below.
+
+# m is the number of training examples
+m = len(x_train)
+print(f"Number of training examples is: {m}")
+
+# Training example x_i, y_i
+
+# To access a value in a Numpy array, one indexes the array with the desired offset. For example the syntax to access location zero of x_train is x_train[0]. Run the next code block below to get the i^th training example.
+
+i = 0 # Change this to 1 to see (x^1, y^1)
+
+x_i = x_train[i]
+y_i = y_train[i]
+print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
+
+# Plotting the data
+
+# You can plot these two points using the scatter() function in the matplotlib library, as shown in the cell below.
+
+# The function arguments marker and c show the points as red crosses (the default is blue dots).
+# You can use other functions in the matplotlib library to set the title and labels to display
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.show()
+
+
+# Model function
+
+# f_{w,b}(x^{(i)}) = wx^{(i)} + b
+
+# The formula above is how you can represent straight lines - different values of w and b give you different straight lines on the plot.
+
+# Let's try to get a better intuition for this through the code blocks below. Let's start with 
+
+w = 100
+b = 100
+
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      w,b (scalar)    : model parameters  
+    Returns
+      y (ndarray (m,)): target values
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+        
+    return f_wb
+
+tmp_f_wb = compute_model_output(x_train, w, b,)
+
+# Plot our model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
+
+w = 200                         
+b = 100    
+
+tmp_f_wb = compute_model_output(x_train, w, b)
+
+# Plot our model prediction
+plt.plot(x_train, tmp_f_wb, c='b',label='Our Prediction')
+
+# Plot the data points
+plt.scatter(x_train, y_train, marker='x', c='r',label='Actual Values')
+
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
+
+# Prediction
+
+# Now that we have a model, we can use it to make our original prediction. Let's predict the price of a house with 1200 sqft. Since the units of x are in 1000's of sqft, x is 1.2.
+
+x_i = 1.2
+cost_1200sqft = w * x_i + b    
+
+print(f"${cost_1200sqft:.0f} thousand dollars")
